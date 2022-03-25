03ecc260ca0cdad18f63b4a5b469939563f0b481
Mark Koenig <35009313+Rakkmark@users.noreply.github.com>
2022-03-25 14:16:02 +0000

    added notes

--- intoCodingCodePlatoonNotes.js
+++ intoCodingCodePlatoonNotes.js
@@ -6,28 +6,56 @@
 // )
 
 
-//Data Types
-//Number 10 - whole, 3.14 float 
+// //Data Types
+// //Number 10 - whole, 3.14 float 
 
-//Typeof Operator 
-console.log(typeof 3.14)
+// //Typeof Operator 
+// console.log(typeof 3.14)
 
 
 
-// String Interpolation
-let firstName = 'Tom'
-let currentCity = 'Chicago'
+// // String Interpolation
+// let firstName = 'Tom'
+// let currentCity = 'Chicago'
 
-// String Concatenation
-console.log("My first name is " + firstName + " and I'm currently living in " + currentCity + ".")
+// // String Concatenation
+// console.log("My first name is " + firstName + " and I'm currently living in " + currentCity + ".")
 
-// String Interpolation - PREFERRED
-console.log(`My first name is ${ firstName } and I'm currently living in ${ currentCity }.`)
+// // String Interpolation - PREFERRED
+// console.log(`My first name is ${ firstName } and I'm currently living in ${ currentCity }.`)
 
 // Embedding Expressions with String Interpolation
-let x = 10
-let y = 5
+// let x = 10
+// let y = 5
 
-console.log(`${ x } times ${ y } = ${ x * y }`)
+// console.log(`${ x } times ${ y } = ${ x * y }`)
 
-console.log(`Is ${ x } greater than ${ y }? This is a ${ x > y } statement.`)
+// console.log(`Is ${ x } greater than ${ y }? This is a ${ x > y } statement.`)
+
+
+
+
+// Conditional Logic (if/else if/else statements)
+// let age = 18
+
+// if (age === 21) {
+//   console.log("Woohoo! Let's go to the bar and celebrate")
+// } else if (age > 21) {
+//   console.log("Let's to the pub.")
+// } else if (age === 18) {
+//   console.log("Sweet! Let's go vote!") // -> This line is executed
+// } else {
+//   console.log("Let's go to the movies.")
+// }
+
+// Multiple Expressions
+// let isRaining = false
+// let onVacation = false
+
+// if (isRaining === true && onVacation === false) {
+//   console.log("Time to do some house work.")
+// } else if (isRaining === true || onVacation === true) {
+//   console.log("Perfect time to go fishing.")
+// } else {
+//   console.log("I'm going to read a book.") // -> This line is executed
+// }
---
+++ namesPractice.js
@@ -0,0 +1,9 @@
+function fullName(firstName, lastName) {
+  // your code here
+  return `${firstName} ${lastName}`
+}
+
+// ---- TESTS - don't change these ---- //
+console.log(fullName('Michael', 'Jordan') === 'Michael Jordan')
+console.log(fullName('Tom', 'Prete') === 'Tom Prete')
+console.log(fullName('Ada', 'Lovelace') === 'Ada Lovelace')
