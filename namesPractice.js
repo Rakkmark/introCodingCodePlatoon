6acf79f0108ec6148ce3956a548485e82130dd49
RakkMark <markkoenig03@gmail.com>
2022-04-01 08:39:07 -0400

    fixing file structure

--- namesPractice.js
+++
@@ -1,9 +0,0 @@
-function fullName(firstName, lastName) {
-  // your code here
-  return `${firstName} ${lastName}`
-}
-
-// ---- TESTS - don't change these ---- //
-console.log(fullName('Michael', 'Jordan') === 'Michael Jordan')
-console.log(fullName('Tom', 'Prete') === 'Tom Prete')
-console.log(fullName('Ada', 'Lovelace') === 'Ada Lovelace')
